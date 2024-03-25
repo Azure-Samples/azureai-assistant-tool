6ccbcf8917c6ce92bfbe550946a42c37d892685c
Jarno Ensio Hakulinen <jhakulin@microsoft.com>
2024-03-25 10:12:47 -0700

    update sample

--- samples/PetTravelPlanChatAssistant/main.py
+++ samples/PetTravelPlanChatAssistant/main.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+
 import json
 import asyncio
 from azure.ai.assistant.management.async_chat_assistant_client import AsyncChatAssistantClient
