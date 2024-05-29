da6d2ad9d59724ab5e38cb759605c5e2f291a951
kaylieee <kaylieleung1@gmail.com>
2024-05-29 11:11:10 -0700

    refactor, add similarity evals, add report gen

--- samples/evals/FileSearch/evaluate.py
+++ samples/evals/FileSearch/evaluate.py
@@ -12,7 +12,9 @@ from azure.ai.assistant.management.conversation_thread_client import Conversatio
 from pathlib import Path
 
 from promptflow.core import AzureOpenAIModelConfiguration
-from promptflow.evals.evaluators import RelevanceEvaluator
+from promptflow.evals.evaluators import RelevanceEvaluator, SimilarityEvaluator
+
+import pandas as pd
 
 RESOURCES_PATH = Path(__file__).parent / 'resources' #path to resources folder
 OUTPUT_PATH = Path(__file__).parent / 'output' #path to output folder
@@ -25,23 +27,60 @@ PROMPTFLOW_CONFIG = AzureOpenAIModelConfiguration(
     azure_deployment=os.environ.get("AZURE_OPENAI_DEPLOYMENT", 'gpt-4o'),
 )
 
+def get_client_args(ai_client_type : str):
+    try:
+        client_args = {}
+        if ai_client_type == "AZURE_OPEN_AI":
+            if os.getenv("AZURE_OPENAI_API_KEY"):
+                # Authenticate using an Azure OpenAI API key
+                # This is generally discouraged, but is provided for developers
+                # that want to develop locally inside the Docker container.
+                print("Using Azure OpenAI with key")
+                client_args["api_key"] = os.getenv("AZURE_OPENAI_API_KEY")
+            else:
+                # Authenticate using the default Azure credential chain
+                # See https://docs.microsoft.com/azure/developer/python/azure-sdk-authenticate#defaultazurecredential
+                # This will *not* work inside a Docker container.
+                print("Using Azure OpenAI with default credential")
+                default_credential = azure.identity.aio.DefaultAzureCredential(
+                    exclude_shared_token_cache_credential=True
+                )
+                client_args["azure_ad_token_provider"] = azure.identity.aio.get_bearer_token_provider(
+                    default_credential, "https://cognitiveservices.azure.com/.default"
+                )
+        elif ai_client_type == "OPEN_AI":
+            # Authenticate using an OpenAI API key
+            if os.getenv("OPENAI_API_KEY"):
+                print("Using OpenAI with key")
+                client_args["api_key"] = os.getenv("OPENAI_API_KEY")
+            else:
+                raise Exception("OpenAI API key not found.")
+        return client_args
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return {}
+
 def initialize_assistant_client():
-    with open(str(RESOURCES_PATH / 'config/eval_file_search_assistant_assistant_config.yaml'), "r") as file:
-        config = yaml.safe_load(file)
-    client = AssistantClient.from_yaml(yaml.dump(config))
+    with open(str(RESOURCES_PATH / 'config/eval_file_search_assistant_assistant_config.yaml'), "r") as f:
+        config = yaml.safe_load(f)
+    client_args = get_client_args(config["ai_client_type"])
+    client = AssistantClient.from_yaml(yaml.dump(config), **client_args)
     return client
 
-def evaluate_file_search_assistant_client_relevance():
+def evaluate_file_search_assistant_client(run_relevance: bool, run_similarity: bool):
+    #initialize
     client = initialize_assistant_client()
-    questions_store, responses_store, citations_store, scores_store = [], [], [], []
-
-    #initialize thread
     thread_client = ConversationThreadClient.get_instance(AIClientType[CLIENT_TYPE])
     thread_name = thread_client.create_conversation_thread()
+    results = {
+        "question": [],
+        "ground truth": [],
+        "response": [],
+        "citation": [],
+    }
 
-    #pass assistant questions and eval answers
+    #pass assistant questions and get responses
     questions = str(RESOURCES_PATH / "assistant_test_dataset" / "questions.txt")
-    evaluator = RelevanceEvaluator(PROMPTFLOW_CONFIG)
     with open(questions, 'r') as f:
         for line in f:
             if line.startswith("Question:"):
@@ -56,19 +95,68 @@ def evaluate_file_search_assistant_client_relevance():
                 response = last_message.content.split("\n")[0].split("[0]")[0].strip()
                 citations = []
                 for citation in last_message.file_citations:
-                    citations.append(str(RESOURCES_PATH / "assistant_test_dataset" /citation.file_name))
-                
-                #evaluate response
-                eval_context = ""
-                for citation in citations:
-                    with open(citation, 'r') as f:
-                        data = json.load(f)
-                        eval_context += json.dumps(data) + "\n"
-                if eval_context == "":
-                    eval_context = "no context available"
-                evaluation = evaluator(
-                    question=question,
-                    answer=response,
-                    context=eval_context,
-                )
-                score = evaluation.get('gpt_relevance') > 0.0
+                    citations.append(citation.file_name)
+
+                #store results
+                results["question"].append(question)
+                results["response"].append(response)
+                results["citation"].append(citations)
+
+            elif line.startswith("Expected Answer:"):
+                answer = line.split(":")[1].strip()
+                results["ground truth"].append(answer)
+        
+        #run necessary evals
+        if run_relevance:
+            print("running relevance evals")
+            results["relevance_scores"] = run_relevance_evals(results.get("question"), results.get("response"), results.get("citation"))
+        if run_similarity:
+            print("running similarity evals")
+            results["similarity_scores"] = run_similarity_evals(results.get("question"), results.get("response"), results.get("ground truth"))
+
+    return results
+
+def run_relevance_evals(questions: list, answers: list, citations: list):
+    scores = []
+    evaluator = RelevanceEvaluator(PROMPTFLOW_CONFIG)
+
+    for i in range(len(questions)):
+        evaluation_context = ""
+        for citation in citations[i]:
+            with open(str(RESOURCES_PATH / "assistant_test_dataset" / citation), 'r') as f:
+                data = json.load(f)
+                evaluation_context += json.dumps(data) + "\n"
+        if evaluation_context == "":
+            evaluation_context = "no context available"
+
+        evaluation = evaluator(
+            question=questions[i],
+            answer=answers[i],
+            context=evaluation_context,
+        )
+        scores.append(evaluation.get('gpt_relevance'))
+    return scores
+
+def run_similarity_evals(questions: list, answers: list, ground_truths: list):
+    scores = []
+    evaluator = SimilarityEvaluator(PROMPTFLOW_CONFIG)
+
+    for i in range(len(questions)):
+        evaluation = evaluator(
+            question=questions[i],
+            answer=answers[i],
+            ground_truth=ground_truths[i],
+        )
+        scores.append(evaluation.get('gpt_similarity'))
+    return scores
+
+def display_results(results):
+    df = pd.DataFrame(results)
+    df.to_csv(str(OUTPUT_PATH / "report.csv"), index=False)
+
+def main():
+    results = evaluate_file_search_assistant_client(True, True)
+    display_results(results)
+
+if __name__ == "__main__":
+    main()  
